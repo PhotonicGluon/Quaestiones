07751319f648ca93267fa7d8dbc77ec6cab09e03
Ryan-Kan <25820201+Ryan-Kan@users.noreply.github.com>
2021-01-15 17:29:21 +0800

    Update Input Generation Code Checker

--- Other Files/Check Input Generation Code.py
+++ Other Files/Check Input Generation Code.py
@@ -2,7 +2,7 @@
 Check Input Generation Code.py
 
 Created on 2020-12-30
-Updated on 2020-12-30
+Updated on 2021-01-15
 
 Copyright © Ryan Kan
 
@@ -17,33 +17,34 @@ Description: Checks whether the input generation code that you have written
 INPUT_GENERATION_FUNCTION = """REPLACE THIS LINE WITH YOUR FUNCTION"""
 
 # Run the input generation code
-exec(INPUT_GENERATION_FUNCTION)
+globalsDict = {}
+exec(INPUT_GENERATION_FUNCTION, globalsDict)
 
 # CHECKS
 # Check to see if the function actually exists
 try:
-    input_generation()
+    outputs = globalsDict["input_generation"]()
 except NameError:
     raise NameError("Your input generation code does not contain a function "
                     "called `input_generation`.")
 
 # Check to see if it returns exactly two things
-try:
-    input_, answer = input_generation()
-except ValueError as e:
-    raise AssertionError("The `input_generation` function DOES NOT return TWO "
-                         "things.")
+assert len(outputs) == 2, "The `input_generation` function DOES NOT return " \
+                          f"TWO things. (It returned {len(outputs)} things)."
+
+# Split the outputs into the input and the answer
+input_, answer = outputs
 
 # Check if the function returns the correct types of things
 if not isinstance(input_, str):
     raise ValueError("The input that is returned by the `input_generation` "
-                     "function is NOT a string. Returned a "
-                     f"{type(input_)}:\n{input_}")
+                     "function is NOT a string. Returned a/an "
+                     f"`{str(type(input_))[8:-2]}`:\n{input_}")
 
 if not isinstance(answer, str):
     raise ValueError("The answer that is returned by the `input_generation` "
-                     "function is NOT a string. Returned a "
-                     f"{type(answer)}:\n{answer}")
+                     "function is NOT a string. Returned a/an "
+                     f"`{str(type(answer))[8:-2]}`:\n{answer}")
 
 # OUTPUT
 # Report that everything went well
