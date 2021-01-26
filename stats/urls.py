9b7f25bbb29d7b513c1716ade364dacec5e80ffa
Ryan-Kan <25820201+Ryan-Kan@users.noreply.github.com>
2021-01-26 10:50:00 +0800

    Allow Staff to Disable the Leaderboard Page

--- stats/urls.py
+++ stats/urls.py
@@ -2,7 +2,7 @@
 urls.py
 
 Created on 2021-01-24
-Updated on 2021-01-25
+Updated on 2021-01-26
 
 Copyright © Ryan Kan
 
@@ -12,11 +12,14 @@ Description: The file which contains the URLconf for the `stats` app.
 # IMPORTS
 from django.urls import path
 
+from Quaestiones.settings import ENABLE_LEADERBOARD
+from Quaestiones.views import error404
 from stats import views
 
 # URL CONFIG
 app_name = "stats"
 
 urlpatterns = [
-    path("leaderboard/", views.leaderboard_view, name="leaderboard")
+    path("leaderboard/", views.leaderboard_view if ENABLE_LEADERBOARD else error404,
+         {"exception": "404"} if not ENABLE_LEADERBOARD else {}, name="leaderboard")
 ]
